a402dd1e2305e96a42448dc0fdd0a1687a0c5105
Phika <phikaplus@gmail.com>
2019-01-26 10:30:16 -0800

    Create play_mp3_mp4.py

---
+++ Level2_Examples/play_mp3_mp4.py
@@ -0,0 +1,15 @@
+# OS: windows
+# Python: 3.6
+
+import os
+
+# open mp3 file
+os.system("F:\\Phika\\Music\\Heisenberg.mp3")
+# OR
+os.system("start F:\Phika\Music\Heisenberg.mp3")
+
+
+# open mp4 file
+os.system("F:\\Phika\\Movie\\Her.MP4")
+# OR
+os.system("start F:\Phika\Movie\Her.MP4")
